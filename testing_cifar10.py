077a0e4b49d7369a9afa60661ae419a9952aaf7f
ghma <ghma@GehuadeMacBook-Pro.local>
2022-09-08 16:19:24 +0800

    updates

--- testing_cifar10.py
+++ testing_cifar10.py
@@ -1,8 +1,8 @@
 '''
 Author: ----
 Date: 2022-06-14 19:48:48
-LastEditors: ----
-LastEditTime: 2022-09-05 15:10:42
+LastEditors: GhMa
+LastEditTime: 2022-09-08 16:16:41
 '''
 import torch
 import torch.nn as nn
@@ -200,52 +200,6 @@ class Attacker:
         self.opt = None
         self.type = type  # valid options: [fgsm, do] for 
                           # fast gradient sign method, direct optimzation 
-
-    def inp_grad_calc(x, labels, model, loss_func):
-        r"""
-        Forked from https://github.com/ssharmin/spikingNN-adversarial-attack
-
-        Calculates the gradient of error with respect to input (del_loss/del_input)
-        :param x: input
-        :param labels: The labels used to calculate the loss or error
-        :param model: Source model
-        :loss_func: Loss function used to calculate the error or loss
-        """
-        inputs = x.clone().detach()
-        inputs.requires_grad_(True)
-        inputs.grad = None       
-        out_channel = model.module.features[0].weight.size()[0]  
-        in_channel = model.module.features[0].weight.size()[1]
-        weight_new = np.zeros((model.module.features[0].weight.size()))
-        for i in range(out_channel):
-            for j in range(in_channel):
-                weight_new[i, j, 0, 0:] = np.flip(
-                    model.module.features[0].weight.detach().cpu(
-                    ).numpy()[i, j, 2, 0:]
-                )
-                weight_new[i, j, 1, 0:] = np.flip(
-                    model.module.features[0].weight.detach().cpu(
-                    ).numpy()[i, j, 1, 0:]
-                )
-                weight_new[i, j, 2, 0:] = np.flip(
-                    model.module.features[0].weight.detach().cpu(
-                    ).numpy()[i, j, 0, 0:]
-                )
-        weight_new = np.transpose(weight_new, (1, 0, 2, 3))
-        weight_rotate = torch.from_numpy(weight_new).float().cuda()
-        
-        output, input_spike_count, grad_mem_conv1 = model(inputs, 0, True)
-        output = output/model.module.timesteps
-            
-        error = loss_func(output, labels)
-        error.backward()        
-        grad_mem_conv1 = model.module.grad_mem1
-        inp_grad = F.conv2d(grad_mem_conv1, weight_rotate, padding=1)
-        # reset        
-        inputs.requires_grad_(False)
-        inputs.grad = None
-        model.module.zero_grad()
-        return inp_grad
     
     def fgsm_attack(self, image, epsilon, data_grad):
         r"""
